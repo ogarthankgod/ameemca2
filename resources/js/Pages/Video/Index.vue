0f347a972a66c94976e23987650c71a97a1478ad
Ogar Thankgod Odeh <ogarthankgododeh@gmail.com>
2024-08-24 08:16:01 +0100

    Patches and Fixes towards update and database reconfiguration(x)

--- resources/js/Pages/Video/Index.vue
+++ resources/js/Pages/Video/Index.vue
@@ -4,11 +4,43 @@ import SectionHead from "@/Layouts/SectionHead.vue";
 import { Head, Link } from "@inertiajs/vue3";
 
 defineOptions({
-    layout: AuthenticatedLayout,
+  layout: AuthenticatedLayout,
 });
 </script>
 
 <template>
-    <Head title="Video Conference" />
-    <SectionHead text="Video Conference" />
+  <Head title="Video Conference" />
+  <SectionHead text="Video Conference" />
+  <div class="bg-white rounded-md p-5 mb-5">
+    <!-- Modal toggle -->
+    <button
+      data-modal-target="crud-modal"
+      data-modal-toggle="crud-modal"
+      class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mb-0"
+      type="button"
+    >
+      Start a Session
+    </button>
+  </div>
+
+  <div class="grid grid-cols-1 mb-7 opacity-50">
+    <div
+      class="flex items-center p-4 mb-4 text-sm text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800"
+      role="alert"
+    >
+      <svg
+        class="flex-shrink-0 inline w-4 h-4 me-3"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+      >
+        <path
+          d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"
+        />
+      </svg>
+      <span class="sr-only">Info</span>
+      <div><span class="font-medium">!!</span> Background Jobs is still Compiling.</div>
+    </div>
+  </div>
 </template>
