ff173e142e5fcb4fdb998539d6c4d681cc78ee42
Ogar Thankgod Odeh <ogarthankgododeh@gmail.com>
2024-08-27 06:45:01 +0100

    Loan Request Fixes..

--- resources/js/Pages/Loan/LoanRequest.vue
+++ resources/js/Pages/Loan/LoanRequest.vue
@@ -186,13 +186,16 @@ const loanRequestSubmit = async () => {
       </div>
 
       <button
-        v-if="loanRequestForm.processing"
         :disabled="loanRequestForm.processing"
         :class="{ 'opacity-25': loanRequestForm.processing }"
         type="submit"
         class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       >
-        <v-icon name="pr-spinner" class="animate-spin"></v-icon>
+        <v-icon
+          v-if="loanRequestForm.processing"
+          name="pr-spinner"
+          class="animate-spin"
+        ></v-icon>
 
         Apply Loan
       </button>
